c1208b29d121ef6de4e3f017a46546150557ba6c
Anas-Rabeea <AnasRabeea001@gmail.com>
2025-08-30 15:15:29 +0300

    First Commit

---
+++ LinearSearch.java
@@ -0,0 +1,12 @@
+
+
+public class LinearSearch
+{
+    // A Practice for Linear Search Algo
+    // Time Complexity = O(n)
+    public static void main(String[] args)
+    {
+            
+    }
+
+}
